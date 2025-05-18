5e22281c748d72ed914d6cc933e7eb9c41ab6228
Darren Nicholas Suwito <145737442+darrennicholass@users.noreply.github.com>
2025-05-18 09:28:44 +0700

    Update models.py

--- models.py
+++ models.py
@@ -3,19 +3,41 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+class Savings(db.Model):
+    __tablename__ = 'savings'
+    savings_trans_id = db.Column(db.Integer, primary_key=True)
+    total_saved = db.Column(db.Float, default=0.0)
+
+    # One-to-many (one savings to many users)
+    users = db.relationship('User', backref='savings', lazy=True)
+
+
+class Budget(db.Model):
+    __tablename__ = 'budget'
+    budget_id = db.Column(db.Integer, primary_key=True)
+    total_budget = db.Column(db.Float, default=0.0)
+
+    # One-to-many (one budget to many users)
+    users = db.relationship('User', backref='budget', lazy=True)
+
+
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    income = db.Column(db.Float, nullable=False, default=0.0)
-    splitting = db.Column(db.Integer, nullable=False, default=0)  # Saving percentage
+    password = db.Column(db.String(100), nullable=False)  # Hashed password
+    income = db.Column(db.Float, nullable=False)
+    splitting = db.Column(db.Integer, nullable=False)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    savings_id = db.Column(db.Integer, db.ForeignKey('savings.savings_trans_id'), nullable=True)
+    budget_id = db.Column(db.Integer, db.ForeignKey('budget.budget_id'), nullable=True)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    def set_password(self, password_plain):
+        self.password = generate_password_hash(password_plain)
+
+    def check_password(self, password_input):
+        return check_password_hash(self.password, password_input)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f"<User {self.username}>"
