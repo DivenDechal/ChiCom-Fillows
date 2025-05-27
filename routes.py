34c1e6c5bf7c7a65bd3a8a5203f03d2c5d5f1263
Darren Nicholas Suwito <145737442+darrennicholass@users.noreply.github.com>
2025-05-27 21:20:41 +0700

    Update routes.py for adding transaction

--- routes.py
+++ routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, session, redirect, url_for
+from datetime import datetime
+from models import Savings, db
 
 BP = Blueprint('BP', __name__)
 
@@ -24,3 +26,40 @@ def saving():
     if 'user_id' not in session:
         return redirect(url_for('BP.login'))
     return render_template('Saving.html')
+    
+@BP.route('/savings', methods = [ 'GET', 'POST'])
+def savings() :
+    if 'user_id' not in session:
+        return redirect(url_for('BP.login'))
+
+    if request.method == 'POST':
+        try:
+            curr_savings = float(request.form.get("amount")
+            transaction = request.form.get("transactionType")
+            date = datetime.now(tz = "id")
+            detail = request.form.get("category")
+            print(curr_savings, transaction,date, detail)
+            
+
+
+            add_transaction = Savings(
+                curr_savings = curr_savings,
+                transaction = transaction,
+                detail = detail,
+                date = date
+            )
+
+            db.session.add(add_transaction)
+            db.session.commit()
+            print("new transaction added!")
+            return render_template('savings.html)
+            
+
+        except Exception as e:
+            print("error add transaction")
+            return "error add transaction"
+
+    else:
+        return render_template('savings.html')
+    
+    
